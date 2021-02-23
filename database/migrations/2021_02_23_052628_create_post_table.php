8752a4f87006813bfce19d1dff6dbbbd2ed7c5f3
MukhammedAli <190103412@stu.sdu.edu.kz>
2021-02-23 11:46:03 +0600

    Creating post table by migration

---
+++ database/migrations/2021_02_23_052628_create_post_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post', function (Blueprint $table) {
+          $table->id('ID'); 
+           $table->string('title');
+           $table->string('body');
+           $table->timestamp('timestamp');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post');
+    }
+}
