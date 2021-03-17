4a4d4d8bc00af847aff523dfb915ceeafb855357
MukhammedAli <190103412@stu.sdu.edu.kz>
2021-03-18 01:07:50 +0600

    Create /posts/details API

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -14,4 +14,16 @@ class ApiController extends Controller
         $posts = Post::all();
         return response($posts,200);
     }
+    /*
+        Return posts with appropriate id number
+    */
+        public function get_posts(Request $request)
+        {
+            $post_id = Post::find($request->post_id);
+            if($post_id == null)
+            {
+                return response(['message' => 'There is no posts you are looking for'],404);   
+            }
+            return response($post_id,200);
+        }
 }
--- routes/api.php
+++ routes/api.php
@@ -18,3 +18,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 Route::get('/posts',[ApiController::class,'index']);
+Route::post('/posts/details',[ApiController::class, 'get_posts']);//id of posts post_id
+
+
+
